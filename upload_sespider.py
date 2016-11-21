5c8ed487d91eaaccd526c26055b291b857b3b11a
qc179 <179313554@qq.com>
2016-11-21 10:22:11 +0800

    refactor 重构到python3并调整了style

--- upload_sespider.py
+++ upload_sespider.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-#filename:upload_sespider.py
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# filename:upload_sespider.py
 
 import os
 import time
 import sys
 from collections import deque
 
-with open('upload_sespider_ip.cfg','rb') as setip:
+with open('upload_sespider_ip.cfg', 'r') as setip:
     iplist = setip.readlines()
     ip = []
     for i in range(len(iplist)):
@@ -18,13 +18,13 @@ with open('upload_sespider_ip.cfg','rb') as setip:
         elif iplist[i] == '\r':
             pass
         else:
-            iplist[i] = iplist[i].replace('\r','')
-            iplist[i] = iplist[i].replace('\n','')
-            iplist[i] = iplist[i].replace(' ','')
+            iplist[i] = iplist[i].replace('\r', '')
+            iplist[i] = iplist[i].replace('\n', '')
+            iplist[i] = iplist[i].replace(' ', '')
             ip.append(iplist[i])
-print ip
+print(ip)
 
-with open('tencentpasswd.cfg','rb') as settc:
+with open('tencentpasswd.cfg', 'r') as settc:
     tclist = settc.readlines()
     tc = []
     for i in range(len(tclist)):
@@ -35,38 +35,40 @@ with open('tencentpasswd.cfg','rb') as settc:
         elif tclist[i] == '\r':
             pass
         else:
-            tclist[i] = tclist[i].replace('\r','')
-            tclist[i] = tclist[i].replace('\n','')
-            tclist[i] = tclist[i].replace(' ','')
+            tclist[i] = tclist[i].replace('\r', '')
+            tclist[i] = tclist[i].replace('\n', '')
+            tclist[i] = tclist[i].replace(' ', '')
             tc.append(tclist[i])
-print tc
+print(tc)
 
 if len(ip) == 0:
-    print u'没有获取到IP'
+    print('没有获取到IP')
     sys.exit()
 elif len(tc) == 0:
-    print u'没有获取到帐号'
+    print('没有获取到帐号')
     sys.exit()
 elif len(ip) != len(tc):
-    print u'IP的数量和帐号数量不一致,请核对后再试'
+    print('IP的数量和帐号数量不一致,请核对后再试')
     sys.exit()
 else:
     pass
 
 tcque = deque(tc)
-#spiderbase.cfg webmon.tencent.user=
-#sespider.cfg SETencentMblog=
-#scp -pr ~/spiders/sespider/ appadmin@211.149.199.70:~/
-#sed -i "s/^spider_group=.*/spider_group=888/g" ~/spiders/sespider_888/sespider.cfg
+# spiderbase.cfg webmon.tencent.user=
+# sespider.cfg SETencentMblog=
+# scp -pr ~/spiders/sespider/ appadmin@211.149.199.70:~/
+# sed -i "s/^spider_group=.*/spider_group=888/g" ~/spiders/sespider_888/sespider
+# .cfg
 
-start = raw_input('请确认IP和帐号都是否正确,若不正确,可直接按回车键退出\n确认后请输入一个起始Gid:\n')
+start = input("请确认IP和帐号都是否正确,若不正确,可直接按回车键退出\n"
+              "确认正确后请输入一个起始Gid:\n")
 
 if start.isdigit():
     pass
 else:
     sys.exit()
 
-log = open('upload.log','a+')
+log = open('upload.log', 'a+')
 ctime = time.strftime('%Y%m%d %H%M%S')
 log.write('\n')
 log.write(ctime)
@@ -74,31 +76,39 @@ log.write('\n')
 
 for i in ip:
     itc = tcque.popleft()
-    print i+':sespider_'+str(start)+':'+itc
+    print(i + ':sespider_' + str(start) + ':' + itc)
 
-    mvspider = 'mv ~/upload/spiders/sespider_* ~/upload/spiders/sespider_'+str(start)
+    mvspider = 'mv ~/upload/spiders/sespider_* ~/upload/spiders/sespider_' + \
+               str(start)
     os.system(mvspider)
 
-    sedgroup = 'sed -i "s/^spider_group=.*/spider_group='+str(start)+'/g" ~/upload/spiders/sespider_'+str(start)+'/sespider.cfg'
+    sedgroup = 'sed -i "s/^spider_group=.*/spider_group=' + str(start) + \
+               '/g" ~/upload/spiders/sespider_' + str(start) + '/sespider.cfg'
     os.system(sedgroup)
 
-    sedtc = 'sed -i "s/^SETencentMblog.*/SETencentMblog='+itc+'/g" ~/upload/spiders/sespider_'+str(start)+'/sespider.cfg'
+    sedtc = 'sed -i "s/^SETencentMblog.*/SETencentMblog=' + itc + \
+            '/g" ~/upload/spiders/sespider_' + str(start) + '/sespider.cfg'
     os.system(sedtc)
 
-    sedgid = 'sed -i "s/^webmon\.sespider\.gid.*/webmon.sespider.gid='+str(start)+'/g" ~/upload/spiders/sespider_'+str(start)+'/spiderbase.cfg'
+    sedgid = 'sed -i "s/^webmon\.sespider\.gid.*/webmon.sespider.gid=' + \
+             str(start) + '/g" ~/upload/spiders/sespider_' + str(start) + \
+             '/spiderbase.cfg'
     os.system(sedgid)
 
-    sedtcuser = 'sed -i "s/^webmon\.tencent\.user.*/webmon.tencent.user='+itc+'/g" ~/upload/spiders/sespider_'+str(start)+'/spiderbase.cfg'
+    sedtcuser = 'sed -i "s/^webmon\.tencent\.user.*/webmon.tencent.user=' + \
+                itc + '/g" ~/upload/spiders/sespider_' + str(start) + \
+                '/spiderbase.cfg'
     os.system(sedtcuser)
 
-    upload = 'scp -pr ~/upload/spiders/sespider_'+str(start)+' appadmin@'+i+':~/'
+    upload = 'scp -pr ~/upload/spiders/sespider_' + str(start) + ' appadmin@' \
+             + i + ':~/'
     os.system(upload)
-    
-    l = i+':sespider_'+str(start)+':'+itc
+
+    l = i + ':sespider_' + str(start) + ':' + itc
     log.write(l)
     log.write('\n')
-    print 'Complete.\n'
+    print('Complete.\n')
 
-    start = int(start)+1
+    start = int(start) + 1
 
 log.close()
